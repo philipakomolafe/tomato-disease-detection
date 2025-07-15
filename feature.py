0dcacf9b711857f74bf4c576777d1f5357ce23b8
Philip Akomolfe <akomolafephilip@gmail.com>
2025-07-15 10:10:20 +0100

    Update project setup

--- feature.py
+++ feature.py
@@ -7,7 +7,7 @@ from loguru import logger
 from tensorflow.keras.models import load_model as tf_load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.applcations.mobilenet_v2 import preprocess_input, decode_predictions
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 from PIL import Image
 import io
 
